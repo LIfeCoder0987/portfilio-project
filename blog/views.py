793efc88749d634becdbeeb1f49d720f99b50b88
bully12 <me@bully12.com>
2019-07-04 00:40:37 +0200

    polishing the blog font page so that it looks great

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
-# Create your views here.
+def all_blogs(request):
+	blogs = Blog.objects
+	return render(request, 'blog/all_blogs.html', {'blogs':blogs})
+
+def blog_detail(request, blog_id):
+	blog_detail = get_object_or_404(Blog, pk=blog_id)
+	return render(request, 'blog/blog_detail.html', {'blog_detail': blog_detail})
